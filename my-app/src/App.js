a392aed42e5e9f15f220ea677989f98082cff52e
changchiyou <changchiyou@gmail.com>
2023-03-19 09:18:36 +0800

    feat: happy dog rotate

---
+++ my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Happy Dog is mine.
+        </p>
+        <a
+          className="App-link"
+          href="https://imgur.com/a/EHpAiNw"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Happy Dog
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
